15fb55a8cf747e2b2916401510c41f31ea51f780
excaoxsal <exca561@gmail.com>
2022-07-22 11:06:00 +0700

    Initial commit

---
+++ ujianwajib/1_array_loop.php
@@ -0,0 +1,15 @@
+<?php
+$aplikasi[1]='gtAkademik';
+$aplikasi[2]='gtFinansi';
+$aplikasi[3]='gtPerizinan';
+$aplikasi[4]='eCampuz';
+$aplikasi[5]='eOviz';
+$x=1;
+while ($x<=5) {
+  echo $aplikasi[$x];
+  echo "<br>";
+
+$x=$x+1;
+}
+
+?>
---
+++ ujianwajib/3_operator_logic.php
@@ -0,0 +1,9 @@
+<?php
+$x=9;
+$y=2;
+echo "hasil pembagian $x dibagi dengan $y adalah ";
+while ($x>$y) {
+    $x=$x-$y;
+}
+echo $x;
+?>
